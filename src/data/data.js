8f448b480423c37a866aca0a38537fad4a32b52d
Mohadeseh Moradi <ms.moradi28@gmail.com>
2024-01-25 10:27:16 +0330

    add data

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+import { AiOutlineDashboard } from "react-icons/ai";
+import { PiHeartbeatLight } from "react-icons/pi";
+import { PiWalletThin } from "react-icons/pi";
+import { SlEnvolopeLetter } from "react-icons/sl";
+import { IoSettingsOutline } from "react-icons/io5";
+
+
+export const itemDashboard = [
+    {id:1, name:"Dashboard", icon:<AiOutlineDashboard/>},
+    {id:2, name:"Workouts", icon:<PiHeartbeatLight/>},
+    {id:3, name:"Meal Plan", icon:<PiWalletThin/>},
+    {id:4, name:"Messages", icon:<SlEnvolopeLetter/>},
+    {id:5, name:"Settings", icon:<IoSettingsOutline/>},
+]
