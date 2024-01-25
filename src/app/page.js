6eacf47bdd64a7d002da804ddf1e853184c03164
Mohadeseh Moradi <ms.moradi28@gmail.com>
2024-01-25 10:27:56 +0330

    import dashboard components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Dashboard from '../component/dashboard/index.jsx'
 
 export default function Home() {
   return (
     <main >
-      <h1 className="">hello</h1>
+      <Dashboard/>
     </main>
   );
 }
