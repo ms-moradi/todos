2ac0fa2ea31fbaaf1059586beef25c86b09601b2
Mohadeseh Moradi <ms.moradi28@gmail.com>
2024-01-26 18:07:12 +0330

    create main layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
-import { Inter } from "next/font/google";
+
+import Dashboard from "@/component/dashboard";
 import "./../style/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+
+
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="w-full h-screen flex ">
+        <Dashboard/>
+        <div id="main-bg" className="w-full h-screen ">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
