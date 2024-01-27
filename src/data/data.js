6530a39053df1099a2a952ed8d08eb0079ac6478
Mohadeseh Moradi <ms.moradi28@gmail.com>
2024-01-27 13:10:10 +0330

    add src to data

--- src/data/data.js
+++ src/data/data.js
@@ -1,14 +1,14 @@
 import { AiOutlineDashboard } from "react-icons/ai";
-import { PiHeartbeatLight } from "react-icons/pi";
-import { PiWalletThin } from "react-icons/pi";
+import { BsListTask } from "react-icons/bs";
+import { FaRegCalendarAlt } from "react-icons/fa";
 import { SlEnvolopeLetter } from "react-icons/sl";
 import { IoSettingsOutline } from "react-icons/io5";
 
 
 export const itemDashboard = [
-    {id:1, name:"Dashboard", icon:<AiOutlineDashboard/>},
-    {id:2, name:"Workouts", icon:<PiHeartbeatLight/>},
-    {id:3, name:"Meal Plan", icon:<PiWalletThin/>},
-    {id:4, name:"Messages", icon:<SlEnvolopeLetter/>},
-    {id:5, name:"Settings", icon:<IoSettingsOutline/>},
+    {id:1, name:"Dashboard", icon:<AiOutlineDashboard/>, src:"/dashboard"},
+    {id:2, name:"All Task", icon:<BsListTask />, src:"/task"},
+    {id:3, name:"Calendar", icon:<FaRegCalendarAlt/>, src:"/calendar"},
+    {id:4, name:"Messages", icon:<SlEnvolopeLetter/>, src:"/message"},
+    {id:5, name:"Settings", icon:<IoSettingsOutline/>, src:"/setting-page"},
 ]
