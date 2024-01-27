cb3a3cdce634d7eaf02081255324b8111019be82
Mohadeseh Moradi <ms.moradi28@gmail.com>
2024-01-27 13:11:16 +0330

    import navbar and dailyTasks

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,21 @@
-import Image from "next/image";
+
+import DailyTasks from "@/component/daily-tasks";
+import Navbar from "@/component/navbar/index";
+
 
 
 export default function Home() {
   return (
-    <main className="">
-      
-      
+    <main className="w-full flex flex-col items-start p-8 ">
+      <div className="w-full ">
+        <Navbar/>
+      </div>
+      <div className="flex justify-start items-center gap-8">
+        <DailyTasks/>
+        <DailyTasks/>
+        <DailyTasks/>
+        <DailyTasks/>
+      </div>
     </main>
   );
 }
