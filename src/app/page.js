b04c4b9fc7c0f4b43f815751784573410e5b9e76
Mohadeseh Moradi <ms.moradi28@gmail.com>
2024-01-29 12:48:03 +0330

    create welcome page

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,15 @@
 
-import DailyTasks from "@/component/daily-tasks";
-import Navbar from "@/component/navbar/index";
+import Link from "next/link";
 
 
 
 export default function Home() {
   return (
-    <main className="w-full flex flex-col items-start p-8 ">
-      <div className="w-full ">
-        <Navbar/>
-      </div>
-      <div className="flex justify-start items-center gap-8">
-        <DailyTasks/>
-        <DailyTasks/>
-        <DailyTasks/>
-        <DailyTasks/>
-      </div>
+    <main className="w-full h-screen bg-green flex flex-col justify-center items-center pt-40">
+      <h1 className="text-6xl text-dark-green  font-bold mb-12">Welcome to Todo App</h1>
+      <Link href="/task">
+        <button className="w-40 h-14 bg-dark-green text-3xl font-semibold text-white rounded-md">Start</button>
+      </Link>
     </main>
   );
 }
